ae5e727ced5e48d010bc6abf95797986653ca67e
Duarte097 <leo_r1089@hotmail.com>
2025-02-10 14:25:00 -0400

    Primeiro commit

---
+++ src/main/java/com/example/sistema_gerenciamento_api/SistemaGerenciamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_gerenciamento_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistema_gerenciamento_api/SistemaGerenciamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_gerenciamento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
