33214b4d81f9129cb667bf6543cd186b559dee85
Duarte097 <leo_r1089@hotmail.com>
2025-02-12 19:36:37 -0400

    Criação do Create User, com encryptação da senha, ainda falta finalizar, pois existe um erro no insert na tabela;

--- src/main/java/com/example/sistema_gerenciamento_api/controllers/UserController.java
+++ src/main/java/com/example/sistema_gerenciamento_api/controllers/UserController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sistema_gerenciamento_api.dto.userDto.CreateUserDto;
 import com.example.sistema_gerenciamento_api.dto.userDto.UpdateUserDto;
+import com.example.sistema_gerenciamento_api.entity.Role;
 import com.example.sistema_gerenciamento_api.entity.User;
+import com.example.sistema_gerenciamento_api.repository.RoleRepository;
+import com.example.sistema_gerenciamento_api.repository.UserRepository;
 import com.example.sistema_gerenciamento_api.service.UserService;
 
-import java.net.URI;
+import jakarta.transaction.Transactional;
+import lombok.experimental.var;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,19 +32,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/createusers")
 public class UserController {
     
-    private UserService userService;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.userService = userService;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
-    @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody CreateUserDto userDto){
+    @PostMapping("/users")
+    @Transactional
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDto userDto){
+        @SuppressWarnings("deprecation")
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+
+        @SuppressWarnings("deprecation")
         var userId = userService.createUser(userDto);
-        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+
+        @SuppressWarnings("deprecation")
+        var user = new User();
+        user.setId_usuarios(userId);
+        user.setNome(userDto.nome());
+        user.setEmail(userDto.email());
+        user.setSenha(passwordEncoder.encode(userDto.senha()));
+        user.setRoles(Set.of(basicRole));
+        userRepository.save(user);
+    
+        //return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
+        @SuppressWarnings("deprecation")
         var user = userService.getUserById(userId);
 
         if(user.isPresent()){
@@ -50,6 +79,7 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<User>> listUsers(){
+        @SuppressWarnings("deprecation")
         var user = userService.listUsers();
 
         return ResponseEntity.ok(user);
